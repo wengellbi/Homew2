dffd06e4033d48e1cd53556784152496bbcc2172
Ibrahim <ibrahim.h.ovale@gmail.com>
2024-02-03 08:52:58 +0300

    hm2

---
+++ main.py
@@ -0,0 +1,19 @@
+class Films:
+    Duration =  "long"
+    Episodes = "less than 2"
+    Plot = "multi-line"
+
+    def __int__(self):
+        self.name = "-"
+        self.year = "1897"
+        self.main_character = "showman"
+
+    class Series:
+        deration = "short"
+        episodes = "more than 4"
+        Plot = "war"
+
+def __init__(self):
+    self.name = "-"
+    self.year = "1968"
+    self.main_character = "concentric"
